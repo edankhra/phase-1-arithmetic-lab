a1bea936f6dd30968fd3e8034104e66d7c1b8c3b
Ekta <edankhra@gmail.com>
2023-04-02 21:05:20 -0400

    completed

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+const num1=31;
+const num2=2;
+let multiply= num1 * num2;
+
+
+console.log(multiply);
+
+let random = Math.floor(Math.random() + 1);
+console.log(random);
+
+let num3 = 19;
+let num4 = 5;
+let mod = (num3%num4) ;
+
+console.log(mod);
+
+
+let max = Math.max(5,10,15,20);
+console.log(max);
